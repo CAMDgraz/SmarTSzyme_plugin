8f170c73bafbff9c5d36cf4db7ce0a4488f278b7
lemongorl <103036172+lemongorl@users.noreply.github.com>
2024-11-20 22:43:26 +0100

    Add files via upload
    __init__.py adds plugin to pymol plugin menu and shows gui plugin window with functional buttons

---
+++ __init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 20 16:46:31 2024
+
+@author: aliaa
+"""
+
+from pymol.plugins import addmenuitemqt
+from . import pmlguiplugin3
+
+
+# Function to launch the plugin
+def launch_plugin():
+    global plugin_window # need this or window crashes immediately after it is launched
+
+    plugin_window = pmlguiplugin3.Plugin()
+    plugin_window.show()
+
+# PyMOL plugin initialization
+def __init_plugin__(app=None):
+    # Add the plugin to the PyMOL menu
+    addmenuitemqt("Aliaa GUI Plugin", launch_plugin)
